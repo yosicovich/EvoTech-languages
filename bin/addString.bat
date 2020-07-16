239e19423f6374e897ebd899456f420215d7d761
yosicovich <yosicovich@gmail.com>
2020-07-16 11:40:40 +0300

    +Add: Support paths with spaces.

--- bin/addString.bat
+++ bin/addString.bat
@@ -1,10 +1,10 @@
 @echo off
 @setlocal
-@set SOURCE_DIR=%1
-@set TARGET_DIR=%2
-@set RESOURCE_DIR=%3
+@set SOURCE_DIR=%~1
+@set TARGET_DIR=%~2
+@set RESOURCE_DIR=%~3
 @set LANG=%4
-ResourceHacker -open %RESOURCE_DIR%\strings_%LANG%.rc -save %TEMP%\strings_%LANG%.res -action compile
-ResourceHacker -open "%SOURCE_DIR%\upgrade\Storage Card\System\data\LangDll%LANG%.dll" -save "%TARGET_DIR%\upgrade\Storage Card\System\data\LangDll%LANG%.dll" -action add -res %TEMP%\strings_%LANG%.res -mask ,,
+ResourceHacker -open "%RESOURCE_DIR%\strings_%LANG%.rc" -save "%TEMP%\strings_%LANG%.res" -action compile
+ResourceHacker -open "%SOURCE_DIR%\upgrade\Storage Card\System\data\LangDll%LANG%.dll" -save "%TARGET_DIR%\upgrade\Storage Card\System\data\LangDll%LANG%.dll" -action add -res "%TEMP%\strings_%LANG%.res" -mask ,,
 del /F /Q %TEMP%\strings_%LANG%.res
 @goto :EOF
--- bin/addStrings.bat
+++ bin/addStrings.bat
@@ -1,18 +1,18 @@
 @echo off
 @setlocal
 @set SCRIPT_PATH=%~dp0
-@set SOURCE_DIR=%1
-@set TARGET_DIR=%2
+@set SOURCE_DIR=%~1
+@set TARGET_DIR=%~2
 @set RESOURCE_DIR=%SCRIPT_PATH%\..
 
-call addString.bat %SOURCE_DIR% %TARGET_DIR% %RESOURCE_DIR% Dut
-call addString.bat %SOURCE_DIR% %TARGET_DIR% %RESOURCE_DIR% Eng
-call addString.bat %SOURCE_DIR% %TARGET_DIR% %RESOURCE_DIR% Fre
-call addString.bat %SOURCE_DIR% %TARGET_DIR% %RESOURCE_DIR% Ger
-call addString.bat %SOURCE_DIR% %TARGET_DIR% %RESOURCE_DIR% Ita
-call addString.bat %SOURCE_DIR% %TARGET_DIR% %RESOURCE_DIR% Por
-call addString.bat %SOURCE_DIR% %TARGET_DIR% %RESOURCE_DIR% Pol
-call addString.bat %SOURCE_DIR% %TARGET_DIR% %RESOURCE_DIR% Rom
-call addString.bat %SOURCE_DIR% %TARGET_DIR% %RESOURCE_DIR% Rus
-call addString.bat %SOURCE_DIR% %TARGET_DIR% %RESOURCE_DIR% Spa
+call addString.bat "%SOURCE_DIR%" "%TARGET_DIR%" "%RESOURCE_DIR%" Dut
+call addString.bat "%SOURCE_DIR%" "%TARGET_DIR%" "%RESOURCE_DIR%" Eng
+call addString.bat "%SOURCE_DIR%" "%TARGET_DIR%" "%RESOURCE_DIR%" Fre
+call addString.bat "%SOURCE_DIR%" "%TARGET_DIR%" "%RESOURCE_DIR%" Ger
+call addString.bat "%SOURCE_DIR%" "%TARGET_DIR%" "%RESOURCE_DIR%" Ita
+call addString.bat "%SOURCE_DIR%" "%TARGET_DIR%" "%RESOURCE_DIR%" Por
+call addString.bat "%SOURCE_DIR%" "%TARGET_DIR%" "%RESOURCE_DIR%" Pol
+call addString.bat "%SOURCE_DIR%" "%TARGET_DIR%" "%RESOURCE_DIR%" Rom
+call addString.bat "%SOURCE_DIR%" "%TARGET_DIR%" "%RESOURCE_DIR%" Rus
+call addString.bat "%SOURCE_DIR%" "%TARGET_DIR%" "%RESOURCE_DIR%" Spa
 @goto :EOF
